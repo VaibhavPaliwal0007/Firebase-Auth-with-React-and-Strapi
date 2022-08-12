f40c9577af2597491a0420fbadb3fe084c176a92
Vaibhav Paliwal <78612380+VaibhavPaliwal0007@users.noreply.github.com>
2022-08-12 20:28:34 +0530

    Update firebase-config.js

--- client/src/config/firebase-config.js
+++ client/src/config/firebase-config.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBy0UE9hHBAlUgp_Y_rspWDIsMpyXjv6_g",
+  apiKey: "",
   authDomain: "auth-486d8.firebaseapp.com",
   projectId: "auth-486d8",
   storageBucket: "auth-486d8.appspot.com",
-  messagingSenderId: "630621169979",
-  appId: "1:630621169979:web:7143345d97fe3b65a8ef1e",
-  measurementId: "G-QDD5927M7Q"
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
